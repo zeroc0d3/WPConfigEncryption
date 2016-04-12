d2db192513264d90adbdb8165b4dad0f3b21adeb
ZeroC0D3 Team <zeroc0d3.0912@gmail.com>
2016-04-12 14:10:37 +0700

    Result Encryption
    Generated by ZeroC0D3 Key Generator

---
+++ z99.php
@@ -0,0 +1,37 @@
+<?php 
+
+/* ====================================================================== */
+/* __________                  _________ _______       .___________       */
+/* \____    /___________  ____ \_   ___ \\   _  \    __| _/\_____  \ Team */
+/*   /     // __ \_  __ \/  _ \/    \  \//  /_\  \  / __ |   _(__  <      */
+/*  /     /\  ___/|  | \(  <_> )     \___\  \_/   \/ /_/ |  /       \     */
+/* /_______ \___  >__|   \____/ \______  /\_____  /\____ | /______  /     */
+/*         \/   \/                     \/       \/      \/        \/      */
+/*                                                                        */
+/*  ZeroC0d3 Team                                                         */
+/*  [ N0th1ng Imp0ss1bl3, Grey Hat Coder ]                                */
+/*  ------------------------------------------------                      */
+/*  http://pastebin.com/u/zeroc0d3                                        */
+/*  https://www.nulled.io/user/123090-zeroc0d3/                           */
+/*                                                                        */
+/* =============== DESCRIPTION FILE ===================================== */
+/*                                                                        */
+/*  Module  : ZEROC0D3 WORDPRESS CONFIG ENCRYPTION                        */
+/*  Author  : ZEROC0D3 Team                                               */
+/*                                                                        */
+/*  DO NOT REMOVE OR MODIFIED THIS FILE, OTHERWISE YOUR CONFIG SCRIPT     */
+/*  WILL NOT WORKING. THIS FILE IS AUTO GENERATE BY SCRIPT GENERATOR      */
+/*                                                                        */
+/* ====================================================================== */
+
+// FILE: z99.php 
+
+define('ZEROC0D3_KEY', 'QnBQSER1UVM0d0F2bUNWSVlMZXRPTWhYVXlmYmdUcWM=');
+define('ZEROC0D3_VER', 'OEFTMWluaCtMMWgxNzZMVlZxdk9XdD09VWphbzF3VlFxTERQL25yZ2JWZ1dhdD09');
+define('ZEROC0D3_SERVER', 'cU8xQlNMRkpad0YvREltd21yNDRHaj09Q2FzcVlaRTR1Tnk5aEdnRHF4NWdtTj09');
+define('ZEROC0D3_PORT', 'ZCtycEtYWXhXZ0MxR1Arc2gxTmZVRD09elRFWWxDM2lueGczbk9TbHY3ZklPaj09');
+define('ZEROC0D3_USERNAME', 'c0t4WUtqR2ZScjFicm9mMGhLZUsxTj09V2VSR0JCTFRkeXhWMllJNDBYRlQyRD09');
+define('ZEROC0D3_PASSWORD', 'V29JbkhOeWxmNm1CbUxvNjFtYVhBaj09Y3hYaEVWcFFXVWxQQzFuUTJ6VzA0aj09');
+define('ZEROC0D3_DBNAME', 'Tkh1bmxaZXRpSmVvbTFhZk05NGpadD09dFpxbnBNV1gwWmV1RTROMjd2MXFtdD09');
+
+?>
