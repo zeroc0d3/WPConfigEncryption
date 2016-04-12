2f2e129e7d2836e546f78fb2a04bf0c5bd724d7d
ZeroC0D3 Team <zeroc0d3.0912@gmail.com>
2016-04-12 13:47:07 +0700

    Core Encryption
    ZeroC0D3 Core Encryption

---
+++ z0.php
@@ -0,0 +1,250 @@
+<?php
+global $fYZ4g87Nj, $pzi5gnXVg;
+global $URD41gkDq, $AjF45gcBM, $eDa42ghLl, $key, $kVf30gIUo, $lVS47g9PD, $BNd44gS7o, $cEF48gFDY, $sNz43gE54, $SZP40gkZJ, $ble49g73K;
+global $XLZ18gG1g, $bAN13g609, $LPV14gp9V, $Piq15gG3A, $aEd16gbVY, $mEb17gi81, $qoO19gAUz;
+global $OdO10gEiB, $FNGbgKPZ3, $jTtcgMMUE, $JRHdg7niZ, $VcDegoLHH, $poGfgcg4G, $mtU11g8fs;
+global $XnP3g6CaJ, $bZH3bgEIm, $Xbp1Wz9Q;
+global $nob3agmdG;
+global $KGF46grkr, $oGq24gO8N, $bpT23gGZ4;
+global $orV21gLEa, $eSn1fg4kZ, $aYM22gfc5, $Eos1egq1Z, $dkO20g6Z4;
+global $rFR34gbTR, $Ckn1dgOMR;
+global $HgM2fgaUK, $MIG3cgrYi;
+
+$GLOBALS['fYZ4g87Nj'] = "\x5aE\x52\x4f\103\060\1043 Word\160\x72\145\163\163 \103\x6fn\146\151\x67 \x45ncry\160ti\x6f\156";
+$GLOBALS['pzi5gnXVg'] = "v\145\x72 1.8.1.6";
+$GLOBALS['AjF45gcBM'];
+$GLOBALS['eDa42ghLl'] = "\x43\x42\103";
+$GLOBALS['ble49g73K'] = $GLOBALS['pzi5gnXVg'];
+$GLOBALS['lVS47g9PD'] = "db_h\157\x73\164";
+$GLOBALS['BNd44gS7o'] = "\x64\x62_p\x6f\x72\164";
+$GLOBALS['cEF48gFDY'] = "\144b_\x75\x73\145r";
+$GLOBALS['sNz43gE54'] = "\x64\x62_p\141ss\167or\144";
+$GLOBALS['SZP40gkZJ'] = "\x64b_\x6e\141\x6d\145";
+$GLOBALS['pYJ33grm9'] = "\x68\x74\x74\160" . (($_SERVER['SERVER_PORT'] == 443) ? "s://" : "://") . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . "/";
+$GLOBALS['bpT23gGZ4'] = "w\x70-c\157\156f\151\147.\160\x68\x70";
+$GLOBALS['oGq24gO8N'] = "\172\x39\x39.\160h\160";
+
+date_default_timezone_set('Asia/Jakarta');
+@ob_start();
+error_reporting(0);
+@ini_set('display_errors', 'Off');
+@ini_set('log_errors', 'Off');
+@ini_set('html_errors', 'Off');
+@ini_set('safe_mode', 'Off');
+@ini_set('register_globals', 'Off');
+@ini_set('post_max_size', '128M');
+@ini_set('memory_limit', '128M');
+@set_time_limit(0);
+@clearstatcache();
+
+function CMX9gYotI() {
+    echo "<s\164\171\x6ce>";
+    echo ".\x62\154o\x63\x6b { \x77i\144\x74h: 270p\170; \x64\151s\x70\154\x61\171: \151n\154in\145-b\154\157c\x6b; \150\x65ig\150t: 170\160x; pa\144\144\x69\x6eg-\154\145\x66\164: 30\160\170; \160\141\144\144\151\156g-b\x6ft\x74\x6f\155: 30\x70\x78; \x74e\x78t-a\x6c\151\x67\156: \143e\x6e\x74\145\x72; \x76\145\x72\x74i\143a\x6c-\141li\x67n: \164\157\x70; p\157\163\151\x74\151\x6f\156: \162\145\x6c\141\x74i\166e; }  .\x62\154\x6fc\153\x32 { \155\x61\162\x67\x69\156: 0 \x61\165\x74o; \164\x65\x78t-\x61\x6c\151gn: \x63\x65\x6ete\162; }  \164ext\x61re\141, \x69n\160\165\x74, \x73\x65\x6cec\x74 { bo\x72\144\x65r: #777 1p\170 s\157\154\x69\144; \160\x61\144d\151\x6e\x67: 5\x70\x78 0; }  .\150i\x64\x64\x65n\x49\156\x70ut { di\163\x70l\141y: \x69\156\154i\x6e\x65-b\x6c\157ck; \167\x69d\164\150: 240\160\170; }  .\143\x6f\x64\145 { \x64\151\163\x70l\x61\171: no\x6ee; }  .\141tt\x6e { \x63\157\154\x6f\162: #c\060\x30; } .\x62\165\x74\x74\x6f\x6e\x350 { \x77\151\144\164\150: 40%; \150e\x69\x67\150\164: 40\x70\170; \x62\x6fr\144e\162-\x77i\x64\x74h: 1\160\x78; b\x6f\162\144\145\162-c\x6f\154\x6fr: #000000; \146\157\156t-\146a\x6dily: T\x61\150\x6f\155\x61, V\x65r\144a\x6ea, \101ri\141\154, \x48\x65\x6c\x76\145t\151\x63\141, \163\141\x6es-\163\x65\162\x69\x66; f\x6fnt-si\x7a\x65: 11\x70\164; \x66\x6f\156\164-\x77\145igh\164: b\x6fl\x64; f\157\x6et-c\157\154\157\x72: #F\x46\x46F\106F; c\157\154\157\x72: #FF\x46F\x46\x46; \142a\143k\147\162ou\x6e\144-c\x6f\154\x6f\162: #000000; v\x65\x72\x74ica\154-a\x6ci\147n: m\x69\x64\x64\x6ce; }  .\142utto\156\x310\x30 { \167id\x74\x68: 90%; \150\145\x69\147\150\x74: 40p\170; bor\144\x65\162-wi\x64t\150: 1\x70\x78; \142\157\x72\144\x65\x72-\x63\157\154\x6f\x72: #000000; \x66\x6f\156\164-f\x61m\151\154y: T\141h\157\155\141, \x56e\162d\x61\156\141, \101ri\141\x6c, H\145\154\166\145t\x69\143\141, \x73an\x73-\163\145ri\x66; \x66o\156\x74-\x73\x69\172\145: 11\x70\164; \146o\x6et-\167\145i\147\x68t: \142\x6f\154\x64; \146\x6fn\x74-\x63\157\x6c\157\162: #\106F\106F\106\x46; \143o\x6c\x6f\x72: #F\106\x46\106\106F; \x62a\x63kg\162\157und-\143o\154\157\x72: #000000; ve\x72\164i\143al-\x61\154\x69g\x6e: \x6d\x69\x64\144\154e; }  .\x6e\x6f-\x70\x72\151n\164 { d\151\x73\160\154a\x79: \x6e\157\156e; }  .d\x69\x73abl\x65 { \146\157\x6e\164-f\x61\x6d\151l\x79: T\x61\150\157\x6d\141, \x56\145rd\x61\x6e\x61, Arial, He\x6cve\164\151\143\141, \163\x61\156\163-serif; \x66\x6f\x6e\x74-\163\x69\172e: 11\160\x74; \x66on\x74-\x77\145\151\147ht: b\157\x6c\x64; b\141\x63\x6b\147\x72\x6f\165\x6ed: #\x44FD\x46\x44\106; \143\x6fl\x6f\x72: #000000; }  .\x64\151\163able\x31 { f\157\x6e\x74-\x66\141\155\151\154\x79: T\x61\150\157\155\x61, \x56\x65\162\144a\x6e\141, \101\162\151\141\x6c, \110\x65\x6c\166\x65\164\x69c\x61, \163\x61n\163-\163\145\162i\x66; \x66o\156\x74-\163\151\x7a\x65: 8pt; \146on\164-\x77\x65i\147ht: b\157\x6cd; bac\x6bg\x72o\x75\x6e\x64: #1\x42\x31\1021\x42; \143olor: #000000; }  .\164\x69\x74le { \x66on\x74-\x66a\155\x69\154\x79: \x54a\x68\157\x6d\x61, \126\145rdan\x61, A\162i\141l, He\154v\x65\164i\143\141, sans-\x73e\162i\146; \146on\164-s\151ze: 16pt; \x66\x6f\156\164-\x77\x65i\x67ht: bo\x6c\x64; c\157\x6co\x72: #1\x42\061\x42\x31\102; v\145\162t\151\x63\141\154-\x61l\151\147\156: \155\x69\144\x64l\x65; t\145xt-\141\x6c\151\x67\156: ce\156\x74\145\x72; \x68\145\x69\147\150\164: 50\160x; }  .\164\x69t\x6c\145\061 { f\157\x6e\164-f\x61m\x69\x6c\171: \124\141\x68\x6f\x6d\141, \126e\x72\144\x61\156\x61, \x41r\151\x61\154, H\145\154\x76et\x69ca, \x73ans-\x73e\162if; \146o\x6et-\163i\172e: 14\160\164; fon\x74-\x77\x65\151\147\x68t: \x62\x6f\x6c\x64; \146o\156\x74-col\x6fr: #000000; \142a\143k\x67r\x6f\165\x6e\x64-\x63\157\154\157\162: #\x41FAFA\106; \x76e\x72t\x69\x63\x61\154-\x61\154\x69\x67n: \x6did\144\x6c\145; \164\145\x78\x74-al\x69\x67\156: \143\145nte\162; \x68e\151\x67\150\164: 30\160x; }  .s\164y\x6c\145\x30 { \x66on\x74-f\141\155\151l\171: \124\141\x68o\155\x61, \x56\145\x72\144an\141, A\x72\151a\x6c, H\x65\x6c\x76e\x74i\143a, s\141\x6e\x73-\x73e\162if; \x66on\164-\x73i\x7a\145: 9\160\164; f\x6f\156t-we\x69g\x68t: b\x6fl\144; c\x6fl\157r: #1\x42\061B\x31B; \166e\x72\164i\143\141\154-\x61\154\x69\x67\x6e: \155\151\x64\x64\x6c\145; \164e\170\x74-\x61l\x69g\156: l\x65ft; }  .\163\x74\171\x6c\145\x30:\150\x6f\166\145\162 { bac\x6b\x67\x72\x6f\165\156\144-\x63olor: #\106\106\106F9\071; }  .\163t\x79\154\x651 { \146\x6f\x6e\164-f\141\155i\154y: \124ahom\x61, V\145\x72\x64\x61\156a, \101ria\154, \x48\x65\154\x76\145\x74i\143\x61, sa\156\163-\x73\145\x72\x69f; \146\x6fn\164-\x73i\x7a\145: 7p\164; \x63o\x6cor: #1\102\061\102\061\102; \160\x61d\x64\151\x6e\x67-le\146t: 5p\x78; \160\x61d\x64\151\x6e\x67-\x72i\x67\x68\164: 5\160\x78; \166\145r\164i\x63\x61l-\x61\x6c\x69\147\x6e: \x6d\151\144d\x6c\x65; te\170t-\x61\x6c\151\147n: \154\x65f\x74; }  .\163\x74\x79\154\x651:h\157ve\x72 { \142a\x63\153\147\x72\157\165\x6ed-\x63\x6f\x6c\157r: #\x46FF\106\x39\071; }  .\163\x74y\x6c\x652 { \x66\x6f\156t-\x66a\x6d\151\154y: \"\x4donos\160\x61\143e\", \"\125\x62\x75n\164\x75 \x4d\x6f\x6eo\", \"\x43\x6fur\x69e\x72 \116e\167\", \x54\141\x68o\155a, \126\145\x72\144\x61na, \x41\162i\141\x6c, \x48el\166\145\x74\x69\143\141, \163\x61n\x73-\x73\145r\151\x66; f\157\156\164-\x73i\x7a\x65: 9\160\x74; f\157n\x74-\167e\x69g\150t: \x62\157\154d; co\x6c\x6f\162: #1\x42\x31\x42\x31\102; v\145r\164\x69\x63\x61l-\141l\151\x67n: \x6d\151\x64\144\x6ce; \164e\170\x74-\141\x6ci\x67\x6e: \x6c\145\x66\x74; }  .\x73\164\x79\154\x652:\x68ove\x72 { \142\x61\143\x6b\x67r\x6f\165nd-c\157\154\157\x72: #\x46\106\106\106\x399; }";
+    echo "</\x73\x74\171l\145>";
+}
+
+function lSl35gbT7($PdG31gm7s) {
+    $XTh3egrXc = str_shuffle("\141bc\144\145\x66g\150i\152\153\x6c\x6dno\x70\161\162s\164\165vw\x78yz\101BCD\x45\106\x47H\x49\112\113\x4cM\116\117\x50\121\x52\x53\124U\126W\x58Y\x5a\x30123\0645\x36\x37\x38\x39");
+    $kVs36gk_Z = substr($XTh3egrXc, 1, $PdG31gm7s);
+    return ($kVs36gk_Z);
+}
+
+class qVh0gqGnK
+{
+    private $key, $PpZ2bg1RL, $fYUag2UGE, $kVf30gIUo, $bMB1bg2r3, $ORA1cgP0A;
+    public function qVh0gqGnK($key = "", $kVf30gIUo = "EC\x42") {
+        $this->key = $key;
+        switch ($kVf30gIUo) {
+            case "\105CB":
+                $this->kVf30gIUo = MCRYPT_MODE_ECB;
+                break;
+
+            case "\103\106\x42":
+                $this->kVf30gIUo = MCRYPT_MODE_CFB;
+                break;
+
+            case "CB\103":
+                $this->kVf30gIUo = MCRYPT_MODE_CBC;
+                break;
+
+            default:
+                $this->kVf30gIUo = MCRYPT_MODE_ECB;
+        }
+    }
+    private function AKM25gA6G() {
+        if ($AjF45gcBM > 16) {
+            $this->PpZ2bg1RL = mcrypt_create_iv(mcrypt_get_block_size(MCRYPT_RIJNDAEL_256, $this->kVf30gIUo), MCRYPT_RAND);
+        } 
+        else {
+            $this->PpZ2bg1RL = mcrypt_create_iv(mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, $this->kVf30gIUo), MCRYPT_RAND);
+        }
+    }
+    public function lbr1agNj_($fYUag2UGE) {
+        $this->fYUag2UGE = $fYUag2UGE;
+        $this->AKM25gA6G();
+        if ($AjF45gcBM > 16) {
+            $this->bMB1bg2r3 = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $this->key, $this->fYUag2UGE, $this->kVf30gIUo, $this->PpZ2bg1RL);
+        } 
+        else {
+            $this->bMB1bg2r3 = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $this->key, $this->fYUag2UGE, $this->kVf30gIUo, $this->PpZ2bg1RL);
+        }
+        $this->ORA1cgP0A = base64_encode($this->PpZ2bg1RL) . base64_encode($this->bMB1bg2r3);
+        return $this->ORA1cgP0A;
+    }
+    public function zZL12gCfO($ORA1cgP0A) {
+        $this->ORA1cgP0A = $ORA1cgP0A;
+        if ($AjF45gcBM > 16) {
+            $OSt7g0dZY = substr($this->ORA1cgP0A, 0, 44);
+            $rZJ6glaFc = substr($this->ORA1cgP0A, 44);
+            $this->PpZ2bg1RL = base64_decode($OSt7g0dZY);
+            $this->bMB1bg2r3 = base64_decode($rZJ6glaFc);
+            $this->fYUag2UGE = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $this->key, $this->bMB1bg2r3, $this->kVf30gIUo, $this->PpZ2bg1RL);
+        } 
+        else {
+            $OSt7g0dZY = substr($this->ORA1cgP0A, 0, 24);
+            $rZJ6glaFc = substr($this->ORA1cgP0A, 24);
+            $this->PpZ2bg1RL = base64_decode($OSt7g0dZY);
+            $this->bMB1bg2r3 = base64_decode($rZJ6glaFc);
+            $this->fYUag2UGE = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $this->key, $this->bMB1bg2r3, $this->kVf30gIUo, $this->PpZ2bg1RL);
+        }
+        return $this->fYUag2UGE;
+    }
+    public function COV38gesi($key) {
+        $this->key = $key;
+    }
+    public function EKP28gl0K() {
+        if ($this->ORA1cgP0A != (null)) return $this->ORA1cgP0A;
+        else return false;
+    }
+    public function Ack26gU3s() {
+        if ($this->fYUag2UGE != (null)) return $this->fYUag2UGE;
+        else return false;
+    }
+    public function Rlg29gK4B() {
+        if ($this->PpZ2bg1RL != (null)) return base64_encode($this->PpZ2bg1RL);
+        else return false;
+    }
+    public function KMf27gmaG() {
+        if ($this->bMB1bg2r3 != (null)) return base64_encode($this->bMB1bg2r3);
+        else return false;
+    }
+}
+
+function nPk37gsn6($URD41gkDq, $lVS47g9PD, $BNd44gS7o, $cEF48gFDY, $sNz43gE54, $SZP40gkZJ, $ble49g73K) {
+    global $URD41gkDq, $AjF45gcBM, $eDa42ghLl, $key, $kVf30gIUo, $lVS47g9PD, $BNd44gS7o, $cEF48gFDY, $sNz43gE54, $SZP40gkZJ, $ble49g73K;
+    global $XLZ18gG1g, $bAN13g609, $LPV14gp9V, $Piq15gG3A, $aEd16gbVY, $mEb17gi81, $qoO19gAUz;
+    global $OdO10gEiB, $FNGbgKPZ3, $jTtcgMMUE, $JRHdg7niZ, $VcDegoLHH, $poGfgcg4G, $mtU11g8fs;
+    global $XnP3g6CaJ, $bZH3bgEIm, $Xbp1Wz9Q;
+    global $nob3agmdG;
+    global $KGF46grkr, $MIG3cgrYi;
+    $bZH3bgEIm = microtime(true);
+    $URD41gkDq = base64_encode(str_rot13($URD41gkDq));
+    $XLZ18gG1g = str_rot13(base64_decode($URD41gkDq));
+    $XnP3g6CaJ = new qVh0gqGnK($key = $XLZ18gG1g, $kVf30gIUo = $eDa42ghLl);
+    $bAN13g609 = base64_encode(str_rot13($XnP3g6CaJ->lbr1agNj_($lVS47g9PD)));
+    $LPV14gp9V = base64_encode(str_rot13($XnP3g6CaJ->lbr1agNj_($BNd44gS7o)));
+    $Piq15gG3A = base64_encode(str_rot13($XnP3g6CaJ->lbr1agNj_($cEF48gFDY)));
+    $aEd16gbVY = base64_encode(str_rot13($XnP3g6CaJ->lbr1agNj_($sNz43gE54)));
+    $mEb17gi81 = base64_encode(str_rot13($XnP3g6CaJ->lbr1agNj_($SZP40gkZJ)));
+    $qoO19gAUz = base64_encode(str_rot13($XnP3g6CaJ->lbr1agNj_($ble49g73K)));
+    $Xbp1Wz9Q = microtime(true);
+    $nob3agmdG = date("\x44, \x64-M-Y \110:\x69");
+    return $XLZ18gG1g;
+    return $bAN13g609;
+    return $LPV14gp9V;
+    return $Piq15gG3A;
+    return $aEd16gbVY;
+    return $mEb17gi81;
+    return $qoO19gAUz;
+    return $bZH3bgEIm;
+    return $Xbp1Wz9Q;
+    return $KGF46grkr;
+}
+
+function oqP2agXIo() {
+    global $MIG3cgrYi;
+    $MIG3cgrYi.= "<?p\x68\x70 \n\n";
+    $MIG3cgrYi.= "/* ====================================================================== */\n";
+    $MIG3cgrYi.= "/* __________                  _________ _______       .___________       */\n";
+    $MIG3cgrYi.= "/* \____    /___________  ____ \_   ___ \\\\   _  \\    __| _/\_____  \\ \124ea\x6d */\n";
+    $MIG3cgrYi.= "/*   /     // __ \_  __ \/  _ \/    \  \//  /_\  \  / __ |   _(__  <      */\n";
+    $MIG3cgrYi.= "/*  /     /\  ___/|  | \(  <_> )     \___\  \_/   \/ /_/ |  /       \     */\n";
+    $MIG3cgrYi.= "/* /_______ \___  >__|   \____/ \______  /\_____  /\____ | /______  /     */\n";
+    $MIG3cgrYi.= "/*         \/   \/                     \/       \/      \/        \/      */\n";
+    $MIG3cgrYi.= "/*                                                                        */\n";
+    $MIG3cgrYi.= "/*  Z\145r\x6f\103\060\x64\063 \124\x65\141\x6d                                                         */\n";
+    $MIG3cgrYi.= "/*  [ \116\060th1n\147 I\x6d\160\x30\x73\x73\x31\142\x6c\x33, \107re\x79 \x48a\164 C\x6f\x64e\x72 ]                                */\n";
+    $MIG3cgrYi.= "/*  ------------------------------------------------                      */\n";
+    $MIG3cgrYi.= "/*  ht\164\160://p\141\163\x74\x65\142in.\143\157\155/\165/\x7ae\x72\157\143\060\1443                                        */\n";
+    $MIG3cgrYi.= "/*  \x68t\x74ps://ww\x77.nu\x6c\154\x65\x64.\151o/us\145\x72/123090-z\145r\157c\060\x64\063/                           */\n";
+    $MIG3cgrYi.= "/*                                                                        */\n";
+    $MIG3cgrYi.= "/* =============== \104\105\x53C\x52\111\120\x54\x49\117\x4e \106\x49\114\x45 ===================================== */\n";
+    $MIG3cgrYi.= "/*                                                                        */\n";
+    $MIG3cgrYi.= "/*  \115\x6f\x64\x75l\x65  : \x5a\105\122\x4f\103\x30\x44\x33 \127\117R\104\120RES\x53 \103ONF\x49G E\116\x43\x52Y\120\124ION                        */\n";
+    $MIG3cgrYi.= "/*  \x41\x75th\x6fr  : \x5a\x45R\117\x43\060D3 T\145\x61m                                               */\n";
+    $MIG3cgrYi.= "/*                                                                        */\n";
+    $MIG3cgrYi.= "/*  \104O \x4e\117T RE\x4d\117\x56\x45 \x4f\122 \x4d\117DI\x46\111E\x44 T\110\111\x53 \106\111LE, \x4f\124H\105\122\127\x49S\x45 \131\117UR C\x4f\x4e\x46\111G \123\103\x52\x49\120\124     */\n";
+    $MIG3cgrYi.= "/*  WI\114L \116OT \127\x4fRK\111N\107. THI\123 \106\111\x4cE IS \101U\124\x4f \x47\x45\116ER\x41\x54\105 B\131 \x53C\122\x49\120\124 G\x45N\105\x52\x41T\x4f\x52      */\n";
+    $MIG3cgrYi.= "/*                                                                        */\n";
+    $MIG3cgrYi.= "/* ====================================================================== */\n\n";
+    return MIG3cgrYi;
+}
+
+function zzP39gi0Z() {
+    global $URD41gkDq, $AjF45gcBM, $eDa42ghLl, $key, $kVf30gIUo, $lVS47g9PD, $BNd44gS7o, $cEF48gFDY, $sNz43gE54, $SZP40gkZJ, $ble49g73K;
+    global $XLZ18gG1g, $bAN13g609, $LPV14gp9V, $Piq15gG3A, $aEd16gbVY, $mEb17gi81, $qoO19gAUz;
+    global $OdO10gEiB, $FNGbgKPZ3, $jTtcgMMUE, $JRHdg7niZ, $VcDegoLHH, $poGfgcg4G, $mtU11g8fs;
+    global $XnP3g6CaJ, $bZH3bgEIm, $Xbp1Wz9Q;
+    global $nob3agmdG;
+    global $KGF46grkr, $MIG3cgrYi;
+    oqP2agXIo();
+    $KGF46grkr = $MIG3cgrYi;
+    $KGF46grkr.= "// FI\114\x45: z9\x39.\160h\x70 \n\n";
+    $KGF46grkr.= "\x64\145\146\151n\145('ZEROC0D3_KEY', '$URD41gkDq');\n";
+    $KGF46grkr.= "\144\x65\x66\x69\x6ee('ZEROC0D3_VER', '$qoO19gAUz');\n";
+    $KGF46grkr.= "def\x69\x6e\x65('ZEROC0D3_SERVER', '$bAN13g609');\n";
+    $KGF46grkr.= "\x64\145f\151\156e('ZEROC0D3_PORT', '$LPV14gp9V');\n";
+    $KGF46grkr.= "\144\145\146in\145('ZEROC0D3_USERNAME', '$Piq15gG3A');\n";
+    $KGF46grkr.= "\x64\x65\146\151\156\x65('ZEROC0D3_PASSWORD', '$aEd16gbVY');\n";
+    $KGF46grkr.= "\144e\x66\151n\145('ZEROC0D3_DBNAME', '$mEb17gi81');\n\n";
+    $KGF46grkr.= "?>";
+    return $KGF46grkr;
+}
+
+function LKk3fgOpP($orV21gLEa) {
+    global $orV21gLEa, $eSn1fg4kZ, $aYM22gfc5, $Eos1egq1Z, $dkO20g6Z4;
+    global $rFR34gbTR, $Ckn1dgOMR;
+    $eSn1fg4kZ = $orV21gLEa;
+    if (file_exists($eSn1fg4kZ)) {
+        $aYM22gfc5 = fopen($eSn1fg4kZ, "\x77");
+        $Eos1egq1Z = $aYM22gfc5;
+    } 
+    else {
+        $dkO20g6Z4 = fopen($eSn1fg4kZ, "w");
+        if ($dkO20g6Z4 == false) die("\x55\x6e\141\142\154\x65 \164o c\x72\x65\141\164e \146i\x6ce -> " . $eSn1fg4kZ);
+        $Eos1egq1Z = $aYM22gfc5;
+    }
+}
+
+function qGk32gkYq($orV21gLEa) {
+    global $orV21gLEa, $eSn1fg4kZ, $aYM22gfc5, $Eos1egq1Z, $dkO20g6Z4;
+    global $rFR34gbTR, $Ckn1dgOMR;
+    $eSn1fg4kZ = $orV21gLEa;
+    if (file_exists($eSn1fg4kZ)) {
+        $aYM22gfc5 = fopen($eSn1fg4kZ, "\x72");
+        $Eos1egq1Z = $aYM22gfc5;
+    } 
+    else {
+        $dkO20g6Z4 = fopen($eSn1fg4kZ, "\x72");
+        if ($dkO20g6Z4 == false) die("\x55n\x61\x62\x6c\x65 to r\x65\x61d f\x69\154\x65 -> " . $eSn1fg4kZ);
+        $Eos1egq1Z = $aYM22gfc5;
+    }
+    $rFR34gbTR = fread($dkO20g6Z4, filesize($eSn1fg4kZ));
+    $Ckn1dgOMR = explode("\\", $rFR34gbTR);
+}
+
+function ekV8gb3DG($orV21gLEa) {
+    fclose($orV21gLEa);
+} 
+
+?>
