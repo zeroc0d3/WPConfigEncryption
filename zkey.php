b2434555220392eb4ce133de955be4764b636d94
ZeroC0D3 Team <zeroc0d3.0912@gmail.com>
2016-04-12 13:50:54 +0700

    Key Generator
    ZeroC0D3 Key Generator

---
+++ zkey.php
@@ -0,0 +1,248 @@
+<?php
+require_once ("z0.\x70\150\160");
+global $fYZ4g87Nj, $pzi5gnXVg;
+global $URD41gkDq, $AjF45gcBM, $eDa42ghLl, $key, $kVf30gIUo, $lVS47g9PD, $BNd44gS7o, $cEF48gFDY, $sNz43gE54, $SZP40gkZJ, $ble49g73K;
+global $XLZ18gG1g, $bAN13g609, $LPV14gp9V, $Piq15gG3A, $aEd16gbVY, $mEb17gi81, $qoO19gAUz;
+global $OdO10gEiB, $FNGbgKPZ3, $jTtcgMMUE, $JRHdg7niZ, $VcDegoLHH, $poGfgcg4G, $mtU11g8fs;
+global $XnP3g6CaJ, $bZH3bgEIm, $Xbp1Wz9Q;
+global $nob3agmdG;
+global $KGF46grkr, $oGq24gO8N, $bpT23gGZ4;
+global $orV21gLEa, $eSn1fg4kZ, $aYM22gfc5, $Eos1egq1Z, $dkO20g6Z4;
+global $rFR34gbTR, $Ckn1dgOMR;
+global $HgM2fgaUK, $MIG3cgrYi;
+
+if ($_GET['action'] == "\143he\x63k") {
+    $ble49g73K = $_POST['ble49g73K'];
+    $lVS47g9PD = $_POST['lVS47g9PD'];
+    $BNd44gS7o = $_POST['BNd44gS7o'];
+    $cEF48gFDY = $_POST['cEF48gFDY'];
+    $sNz43gE54 = $_POST['sNz43gE54'];
+    $SZP40gkZJ = $_POST['SZP40gkZJ'];
+    $KGF46grkr = $_POST['KGF46grkr'];
+    if (isset($_POST["lVS47g9PD"])) {
+        switch ($_POST['AjF45gcBM']) {
+            case "16":
+                $URD41gkDq = lSl35gbT7(16);
+                break;
+
+            case "32":
+                $URD41gkDq = lSl35gbT7(32);
+                break;
+
+            default:
+                $URD41gkDq = lSl35gbT7(16);
+        }
+        nPk37gsn6($URD41gkDq, $lVS47g9PD, $BNd44gS7o, $cEF48gFDY, $sNz43gE54, $SZP40gkZJ, $ble49g73K);
+        $HgM2fgaUK = "ZE\122\117\x43\060D\x33 \x4b\145\171 Ge\156\x65\162a\x74e\x64...       [ \x44\x4fNE ]\n";
+    }
+}
+
+if ($_GET['action'] == "\x63\162e\141\x74e") {
+    $ble49g73K = $_POST['ble49g73K'];
+    $lVS47g9PD = $_POST['lVS47g9PD'];
+    $BNd44gS7o = $_POST['BNd44gS7o'];
+    $cEF48gFDY = $_POST['cEF48gFDY'];
+    $sNz43gE54 = $_POST['sNz43gE54'];
+    $SZP40gkZJ = $_POST['SZP40gkZJ'];
+    $KGF46grkr = $_POST['KGF46grkr'];
+    if (isset($_POST["lVS47g9PD"])) {
+        switch ($_POST['AjF45gcBM']) {
+            case "16":
+                $URD41gkDq = lSl35gbT7(16);
+                break;
+
+            case "32":
+                $URD41gkDq = lSl35gbT7(32);
+                break;
+
+            default:
+                $URD41gkDq = lSl35gbT7(16);
+        }
+        nPk37gsn6($URD41gkDq, $lVS47g9PD, $BNd44gS7o, $cEF48gFDY, $sNz43gE54, $SZP40gkZJ, $ble49g73K);
+        $NOP2cgga6 = "Z\105R\117\1030\104\063 \x4b\x65\x79 \107\145\x6ee\x72\141\164\x65\144...       [ \104ONE ]\n";
+        LKk3fgOpP($orV21gLEa = $oGq24gO8N);
+        fputs($Eos1egq1Z, $Tgl3dgYTO = $KGF46grkr);
+        ekV8gb3DG($orV21gLEa = $oGq24gO8N);
+        $jFh2dgOCI = "ZE\x52OC\x30\104\x33 \113\x65\x79 \127\162\151te \124o Fi\x6ce...   [ \104\117\116E ]\n";
+        $FVj2egc7j = "A\x6cl P\x72\x6fces\x73...                  [ D\117\x4e\x45 ]\n";
+        $HgM2fgaUK = $NOP2cgga6 . $jFh2dgOCI . $FVj2egc7j;
+    }
+} ?>
+
+<html>
+<head>
+    <title><?php
+echo $fYZ4g87Nj . "&\x6e\x62\x73p;@&\156bsp;" . $pzi5gnXVg; ?></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <?php
+CMX9gYotI(); ?>
+</head>
+<body>
+<table width="700" border="1" align="center">
+    <form id="form1" name="form1" method="post" action="<?php
+echo basename($_SERVER['PHP_SELF']); ?>?action=check">
+        <tr>
+            <th colspan="3" class="title" scope="col" bgcolor="#AFAFAF">&nbsp;<strong><?php
+echo $fYZ4g87Nj; ?></strong>&nbsp;
+            </th>
+        </tr>
+        <tr>
+            <th scope="row" class="style0" width="250px">&nbsp;ZEROC0D3_VERSION&nbsp;</th>
+            <td class="style0" style="text-align:center" width="5">
+                <center>&nbsp;:&nbsp;</center>
+            </td>
+            <td><input type="text" class="disable" name="ble49g73K" id="ble49g73K" style="width: 350px"
+                       value="<?php
+echo $ble49g73K; ?>" readonly/></td>
+        </tr>
+        <tr>
+            <th scope="row" class="style0" width="250px">&nbsp;ZEROC0D3_KEY&nbsp;(RANDOM)&nbsp;</th>
+            <td class="style0" style="text-align:center" width="5">
+                <center>&nbsp;:&nbsp;</center>
+            </td>
+            <td>
+                <select name="AjF45gcBM" style="width: 350px" onchange="Change(this);" readonly>
+                    <option value="16" selected>RIJNDAEL-128</option>
+                    <option value="32">RIJNDAEL-256</option>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <th scope="row" class="style0" width="250px">&nbsp;ZEROC0D3_SERVER&nbsp;</th>
+            <td class="style0" style="text-align:center" width="5">
+                <center>&nbsp;:&nbsp;</center>
+            </td>
+            <td><input type="text" class="style0" name="lVS47g9PD" id="lVS47g9PD" style="width: 350px"
+                       value="<?php
+echo $lVS47g9PD; ?>"/></td>
+        </tr>
+        <tr>
+            <th scope="row" class="style0" width="250px">&nbsp;ZEROC0D3_PORT&nbsp;</th>
+            <td class="style0" style="text-align:center" width="5">
+                <center>&nbsp;:&nbsp;</center>
+            </td>
+            <td><input type="text" class="style0" name="BNd44gS7o" id="BNd44gS7o" style="width: 350px"
+                       value="<?php
+echo $BNd44gS7o; ?>"/></td>
+        </tr>
+        <tr>
+            <th scope="row" class="style0" width="250px">&nbsp;ZEROC0D3_USERNAME&nbsp;</th>
+            <td class="style0" style="text-align:center" width="5">
+                <center>&nbsp;:&nbsp;</center>
+            </td>
+            <td><input type="text" class="style0" name="cEF48gFDY" id="cEF48gFDY" style="width: 350px"
+                       value="<?php
+echo $cEF48gFDY; ?>"/></td>
+        </tr>
+        <tr>
+            <th scope="row" class="style0" width="250px">&nbsp;ZEROC0D3_PASSWORD&nbsp;</th>
+            <td class="style0" style="text-align:center" width="5">
+                <center>&nbsp;:&nbsp;</center>
+            </td>
+            <td><input type="password" class="style0" name="sNz43gE54" id="sNz43gE54" style="width: 350px"
+                       value="<?php
+echo $sNz43gE54; ?>"/></td>
+        </tr>
+        <tr>
+            <th scope="row" class="style0" width="250px">&nbsp;ZEROC0D3_DBNAME&nbsp;</th>
+            <td class="style0" style="text-align:center" width="5">
+                <center>&nbsp;:&nbsp;</center>
+            </td>
+            <td><input type="text" class="style0" name="SZP40gkZJ" id="SZP40gkZJ" style="width: 350px"
+                       value="<?php
+echo $SZP40gkZJ; ?>"/></td>
+        </tr>
+        <tr>
+            <th colspan="3" style="text-align:left" scope="row">
+                <center>
+                    <input type="submit" name="generate" id="generate" value="GENERATE KEY (PREVIEW)" class="button100"/>
+                </center>
+            </th>
+        </tr>
+    </form>
+    <tr>
+        <th colspan="3" class="title1" scope="col" bgcolor="#AFAFAF">&nbsp;<strong>ZEROC0D3 ENCRYPTION (RIJNDAEL-128 / RIJNDAEL-256) ::
+                RESULTS</strong>
+        </th>
+    </tr>
+    <tr>
+        <th colspan="3" class="style2" scope="row">
+        <textarea name="KGF46grkr" cols="100" rows="10" value="<?php
+echo $KGF46grkr; ?>" readonly><?php
+if ($bZH3bgEIm > 0) {
+    zzP39gi0Z();
+    echo $KGF46grkr;
+} ?>
+        </textarea>
+        </th>
+    </tr>
+    <tr>
+        <th colspan="3" class="disable1" scope="row">&nbsp;</th>
+    </tr>
+    <form id="form2" name="form2" method="post" action="<?php
+echo basename($_SERVER['PHP_SELF']); ?>?action=create">
+        <tr>
+            <th colspan="3" class="title1" scope="col" bgcolor="#AFAFAF">&nbsp;<strong>ZEROC0D3 CONFIG FILE :: BUILD </strong>
+            </th>
+        </tr>
+        <tr>
+            <th scope="row" class="style0" width="250px">&nbsp;ENCRYPT_CONFIG_FILE&nbsp;</th>
+            <td class="style0" style="text-align:center" width="5">
+                <center>&nbsp;:&nbsp;</center>
+            </td>
+            <td><input type="text" class="disable" name="oGq24gO8N" id="oGq24gO8N" style="width: 350px" value="<?php
+echo $oGq24gO8N; ?>" "width: 350px"
+                       title="ZEROC0D3_CONFIG_FILE" readonly/></td>
+        </tr>
+        <tr>
+            <th colspan="3" style="text-align:left" scope="row">
+                <center>
+                    <input type="hidden" class="disable" name="AjF45gcBM" id="AjF45gcBM" style="width: 350px"
+                           value="<?php
+echo $AjF45gcBM; ?>" readonly/>
+                    <input type="hidden" class="disable" name="ble49g73K" id="ble49g73K" style="width: 350px"
+                           value="<?php
+echo $ble49g73K; ?>" readonly/>
+                    <input type="hidden" class="disable" name="lVS47g9PD" id="lVS47g9PD" style="width: 350px"
+                           value="<?php
+echo $lVS47g9PD; ?>" readonly/>
+                    <input type="hidden" class="disable" name="BNd44gS7o" id="BNd44gS7o" style="width: 350px"
+                           value="<?php
+echo $BNd44gS7o; ?>" readonly/>
+                    <input type="hidden" class="disable" name="cEF48gFDY" id="cEF48gFDY" style="width: 350px"
+                           value="<?php
+echo $cEF48gFDY; ?>" readonly/>
+                    <input type="hidden" class="disable" name="sNz43gE54" id="sNz43gE54" style="width: 350px"
+                           value="<?php
+echo $sNz43gE54; ?>" readonly/>
+                    <input type="hidden" class="disable" name="SZP40gkZJ" id="SZP40gkZJ" style="width: 350px"
+                           value="<?php
+echo $SZP40gkZJ; ?>" readonly/>
+                    <input type="hidden" class="style0" name="KGF46grkr" id="KGF46grkr" style="width: 350px"
+                           value="<?php
+echo $KGF46grkr; ?>" readonly title="ZEROC0D3_RESULT"/>       
+                    <input type="submit" name="build" id="build" value="CREATE CONFIG FILE (BUILD)" class="button100"/>
+                </center>
+            </th>
+        </tr>
+    </form>
+    <tr>
+        <th colspan="3" class="disable1" scope="row">&nbsp;</th>
+    </tr>
+    <th colspan="3" class="title1" scope="col" bgcolor="#AFAFAF">&nbsp;<strong>ZEROC0D3 STATUS SYSTEM :: LOGS</strong></th>
+    <tr>
+        <th colspan="3" class="style2" scope="row">
+        <textarea name="logs" cols="100" rows="4" readonly><?php
+if ($bZH3bgEIm > 0) {
+    echo $HgM2fgaUK . "\n";
+} ?></textarea>
+        </th>
+    </tr>
+    <tr>
+        <th colspan="3" class="style1" scope="row">&nbsp;Execution Time (s)
+            : <?php
+echo "<\x73\164ro\x6e\x67>" . ($Xbp1Wz9Q - $bZH3bgEIm) . " | L\141\163\164 A\x63\164iv\151\164\151\x65\163 : " . $nob3agmdG . " </\163\164r\x6f\156g>"; ?></th>
+    </tr>
+</table>
+</body>
+</html>
